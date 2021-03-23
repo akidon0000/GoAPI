3ccd8296e98c3f4017c73756d529184450587dd3
Akihiro Matsuyama <a@art.jp>
2021-03-23 18:46:13 +0900

    整形

--- main.go
+++ main.go
@@ -14,20 +14,6 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-type apiHandler struct {
-}
-
-type User struct {
-	MyAssociation      string
-	PartnerAssociation string
-	Quadkey            string
-	Status             int
-}
-
-func (u User) String() string {
-	return fmt.Sprintf("%s(%d)[%s]", u.MyAssociation, u.PartnerAssociation, u.Quadkey)
-}
-
 
 func main() {
 	// DBに接続
@@ -37,42 +23,26 @@ func main() {
 	// テーブル名の複数形化を無効化します。trueにすると`User`のテーブル名は`user`になります
 	db.SingularTable(true)
 
-	// db.NewRecord(User{Name:"AA", Age:10})
-	// db.NewRecord(User{Name:"BB", Age:20})
-	// // 構造体のインスタンス化
-	// user := User{}
-	// // 挿入したい情報を構造体に与える
-	// user.MyAssociation = "AAA"
-	// user.PartnerAssociation = "BBB"
-	// // INSERTを実行
-	// db.Create(&user)
-
-	// e := echo.New()
-	// handler := apiHandler{}
-	// // 定義した struct を登録
-	// openapi.RegisterHandlers(e, handler)
-	// //e.Logger.Fatal(e.Start(fmt.Sprintf("0.0.0.0:%d", *port)))
-	// e.Logger.Fatal(e.Start(":1323"))
 }
+
 
 // SQLConnect DB接続
 func gormConnect() (database *gorm.DB) {
 	err := godotenv.Load(fmt.Sprintf("../%s.env", os.Getenv("GO_ENV")))
-    if err != nil {
-			panic(err.Error())
-        // .env読めなかった場合の処理
-    }
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Println(".env読み取り成功")
+	}
+
 	DBMS := "mysql"
-	PROTOCOL := "tcp(localhost:3306)" // db:3306 <<< docker-compose.ymlで定義したMySQLのサービス名:ポート
+	PROTOCOL := "tcp(localhost:3306)" // db:3306
 	USER := os.Getenv("DB_ROLE")       // データベース
 	PASS := os.Getenv("DB_PASSWORD")
 	DBNAME := os.Getenv("DB_NAME")
-	fmt.Println(DBNAME)
-	fmt.Println("成功")
 
 	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME //+ "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
 	db, err := gorm.Open(DBMS, CONNECT)
-
 	if err != nil {
 		panic(err.Error())
 	} else {
@@ -80,53 +50,3 @@ func gormConnect() (database *gorm.DB) {
 	}
 	return db
 }
-
-
-
-
-
-// // Userを追加する。ログイン認証なし、idとtokenをkeyChainに保存
-// // (POST /user)
-// func (si apiHandler) AddUser(ctx echo.Context) error {
-// 	//	b := NewUser{}
-// 	// リクエスト Body は echo の APIを利用
-// 	//	ctx.Bind(&b)
-
-// 	return ctx.String(http.StatusOK, "こんにちは! AddUser")
-// }
-
-
-
-
-// // Userを更新する
-// // (PUT /user)
-// func (si apiHandler) UpdateUser(ctx echo.Context, params openapi.UpdateUserParams) error {
-// 	// ビジネスロジック
-// 	// var err error
-// 	// return err
-// 	return ctx.String(http.StatusOK, "UpdateUser")
-// }
-
-// // Userへメッセージを送る
-// // (POST /user/message)
-// func (si apiHandler) PostUserMessage(ctx echo.Context, params openapi.PostUserMessageParams) error {
-// 	// ビジネスロジック
-// 	var err error
-// 	return err
-// }
-
-// // Userを取得する
-// // (GET /user/{userId})
-// func (si apiHandler) GetUserId(ctx echo.Context, userId int64, params openapi.GetUserIdParams) error {
-// 	// ビジネスロジック
-// 	var err error
-// 	return err
-// }
-
-// // Userの相性を取得する
-// // (GET /user/{userId}/affinity)
-// func (si apiHandler) GetUserIdUnbrella(ctx echo.Context, userId int64, params openapi.GetUserIdUnbrellaParams) error {
-// 	// ビジネスロジック
-// 	var err error
-// 	return err
-// }
