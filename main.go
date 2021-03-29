73131169af7d1e16ab11c13b0aed2fc9fa702221
Akihiro Matsuyama <a@art.jp>
2021-03-29 12:47:36 +0900

    mysql接続通信実装v1

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 	"log"
 	// jsonデータを作成
-	"encoding/json"
+	// "encoding/json"
 	// "gen"
 	// "github.com/labstack/echo"
 
@@ -22,41 +22,76 @@ import (
 
 type User struct {
 	gorm.Model
-	uuid                         string `json:"uuid"`
-	my_association        string `json:"my_association"`
-	partner_association string `json:"partner_association"`
-	quadkey                  string `json:"quadkey"`
-	status                      int `json:"status"`
+	Uuid                string
+	My_association      string `gorm:"size:255"`
+	Partner_association string
+	Quadkey             string
+	Status              int
 }
+
+func (u User) String() string {
+	return fmt.Sprintf("%s(%d)", u.My_association, u.Status)
+}
+
+func insert(users []User, db *gorm.DB) {
+	for _, user := range users {
+			db.NewRecord(user)
+			db.Create(&user)
+	}
+}
+
+func findAll(db *gorm.DB) []User {
+	var allUsers []User
+	db.Find(&allUsers)
+	return allUsers
+}
+
+// var uss User
+// Get All User
+// func getUser(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "application/json")
+
+// 	db := gormConnect()
+// 	defer db.Close()
+
+	// db.Find(&uss)
+	// var utest []User
+	// UserEx := User{}
+	// // UserEx.uuid = "123"
+	// db.First(&UserEx)
+
+	// json.NewEncoder(w).Encode(UserEx.my_association)
+  // json.NewEncoder(w).Encode(uss)
+// }
 
 func main() {
 	// DBに接続
-	// db := gormConnect()
-	// // main関数が終わる際にDBの接続を切る
-	// defer db.Close()
+	db := gormConnect()
+	// main関数が終わる際にDBの接続を切る
+	defer db.Close()
+	fmt.Println(findAll(db))
+  user1 := User{
+						Uuid: "12",
+						My_association: "test1",
+						Partner_association: "test1",
+						Quadkey: "1234",
+						Status: 1,}
+  insertUsers := []User{user1}
+  insert(insertUsers, db)
+
+	// rows := db.Query("")
+	// fmt.Println("DB接続成功")
+
 
 
 	r := mux.NewRouter()
 	// localhost:8080一覧を取得
-	r.HandleFunc("/v1/user/all", getUser).Methods("GET")
+	// r.HandleFunc("/v1/user/all", getUser).Methods("GET")
 	// r.HandleFunc("/opening/", showOpeningIndex)
 	log.Fatal(http.ListenAndServe(":8080", r))
-
+	db.Set("gorm:table_options", "ENGINE = InnoDB").AutoMigrate(&User{})
 }
 
-// Get All Books
-func getUser(w http.ResponseWriter, r *http.Request) {
-	db := gormConnect()
-	defer db.Close()
-
-	// var result User
-	UserEx := User{}
-	// UserEx.uuid = "123"
-	db.First(&UserEx)
-
-	w.Header().Set("Content-Type", "application/json")
-  json.NewEncoder(w).Encode(UserEx)
-}
 
 // SQLConnect DB接続
 func gormConnect() (database *gorm.DB) {
